ff2945cd7866dac29975ef3ab26ee9cbd514b289
Diya_Thangada <dthangada@gmail.com>
2022-01-09 22:56:15 -0500

    updated testmapeatcookie

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -2,9 +2,9 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
-public class TestMapEatCookie {
+public class TestMapEatCookie extends TestCase{
 	
-	public void testMapEatCookie() {
+	public void testMapEatCookie() throws FileNotFoundException{
 		//Creating A Map
 		MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
 		NoFrame noFrame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
