6743ac3e4f6dd174c5d7ad1f30cdeedddef3771c
Ambika <amsikri@gmail.com>
2022-01-09 19:07:28 -0500

    Test for valid moves added

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -1,10 +1,22 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import Collections; 
 
 public class TestPacManValidMoves extends TestCase {
 
-	public void testPacManValidMoves() throws FileNotFoundException{
-		return null;
+	public void testPacManValidMoves() {
+		
+		NoFrame nf = new NoFrame(); 
+		ArrayList<Location> vals = new ArrayList<Integer>(); 
+		vals.add((9, 12));
+		vals.add((9, 10));
+		vals.add((10, 11));
+		vals.add((8,11));
+		Collections.sort(vals)
+		PacMan pacman = nf.addPacMan(new Location(9, 11));
+
+		assertTrue(Collections.sort(pacman.get_valid_moves).equals(vals);	
+
 	}
 }
