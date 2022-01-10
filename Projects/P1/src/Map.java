dad51ba0071ba9366b5e148b86184b0fc3e17dcb
Diya_Thangada <dthangada@gmail.com>
2022-01-09 23:03:02 -0500

    updated eatcookie

--- Projects/P1/src/Map.java
+++ Projects/P1/src/Map.java
@@ -71,7 +71,7 @@ public class Map{
 	public JComponent eatCookie(String name) {
 		//update locations, components, field, and cookies
 		//the id for a cookie at (10, 1) is tok_x10_y1
-		if( (getLoc(this.locations.get(name))).equals(Map.Type.COOKIE) ){
+		if( (getLoc(this.locations.get(name)) != null) && (getLoc(this.locations.get(name))).equals(Map.Type.COOKIE) ){
 			Location thisLocation = this.locations.remove(name);
 			JComponent returnValue = this.components.remove(name);
 			HashSet newSet = this.field.get(thisLocation);
