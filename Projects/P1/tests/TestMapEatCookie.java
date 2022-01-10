69610b13974b601112be0487dcfa1b81e4361b52
Diya_Thangada <dthangada@gmail.com>
2022-01-09 21:20:54 -0500

    updated TestMapEatCookie.java

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -5,6 +5,26 @@ import java.io.*;
 public class TestMapEatCookie {
 	
 	public void testMapEatCookie() {
-		return null;
+		//Creating A Map
+		MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+		NoFrame noFrame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+
+		Location location = new Location(15, 35);
+		
+		//Creating Players
+		Ghost ghost = frame.addGhost(new Location(2, 2), "name", Color.red); //Creates a red ghost named "name" at location x,y
+		PacMan pacman = frame.addPacMan(location); //Creates PacMan at location x, y
+		
+		Map map = frame.getMap();
+		if(map.eatCookie("pacman") != null){
+			if( (map.getLoc(location)).contains(Map.Type.COOKIE) ){
+				assertTrue(true);
+			} else {
+				assertTrue(false);
+			}
+		}
+		assertTrue(true);
+		//Start The Game
+		frame.startGame();
 	}
 }
