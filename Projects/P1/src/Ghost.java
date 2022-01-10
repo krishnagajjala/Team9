da33b5c9a63d529968208ac82a197da7efa39827
Diya_Thangada <dthangada@gmail.com>
2022-01-09 20:51:20 -0500

    updated is_pacman_in_range

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -21,7 +21,31 @@ public class Ghost{
 	}
 
 	public boolean is_pacman_in_range() { 
-		return false;
+		Location above = new Location(this.myLoc.x, this.myLoc.y + 1);
+		Location below = new Location(this.myLoc.x, this.myLoc.y - 1);
+		Location left = new Location(this.myLoc.x - 1, this.myLoc.y);
+		Location right = new Location(this.myLoc.x + 1, this.myLoc.y);
+		Location upperLeft = new Location(this.myLoc.x - 1, this.myLoc.y + 1);
+		Location upperRight = new Location(this.myLoc.x + 1, this.myLoc.y + 1);
+		Location lowerLeft = new Location(this.myLoc.x - 1, this.myLoc.y - 1);
+		Location lowerRight = new Location(this.myLoc.x + 1, this.myLoc.y - 1);
+		
+		if(
+		(this.myMap.getLoc(above)).contains(Map.Type.PACMAN) ||
+		(this.myMap.getLoc(below)).contains(Map.Type.PACMAN) ||
+		(this.myMap.getLoc(left)).contains(Map.Type.PACMAN) ||
+		(this.myMap.getLoc(right)).contains(Map.Type.PACMAN) ||
+		(this.myMap.getLoc(upperLeft)).contains(Map.Type.PACMAN) ||
+		(this.myMap.getLoc(upperRight)).contains(Map.Type.PACMAN) ||
+		(this.myMap.getLoc(lowerLeft)).contains(Map.Type.PACMAN) ||
+		(this.myMap.getLoc(lowerRight)).contains(Map.Type.PACMAN) ||
+		(this.myMap.getLoc(this.location)).contains(Map.Type.PACMAN)
+		) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 
 	public boolean attack() {
