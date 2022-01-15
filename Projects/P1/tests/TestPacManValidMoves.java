aadf0f39d8af49bbfad1fccaf16f431c21a4141a
Ambika <amsikri@gmail.com>
2022-01-15 16:12:54 -0500

    Fixed TestPacManValidMoves.java

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -17,8 +17,7 @@ public class TestPacManValidMoves extends TestCase {
                         for(int j = 0; j < 15; j++) {
                                 CookieComponent tok = new CookieComponent(i,j,20);
                                 frame.getMap().add("tok_x"+i+"_y"+j, new Location(i,j), tok, Map.Type.COOKIE);
-                                                  frame.add(tok);
-                                                  tok.setLocation(i, j);  
+                                	tok.setLocation(i, j);  
                         }
                 }
 
