d0988405bc6250d26bd23c80385f2875466a7248
Christopher Hasbani <chasbani@umd.edu>
2022-01-16 20:59:07 -0500

    noframe for testmapMove

--- Projects/P1/tests/TestMapMove.java
+++ Projects/P1/tests/TestMapMove.java
@@ -6,19 +6,18 @@ public class TestMapMove extends TestCase {
 
 	public void testMapMove() throws FileNotFoundException{
 		
-		MainFrame frame = new MainFrame();
+		NoFrame frame = new NoFrame();
 	
 
 
-                PacMan pacman = frame.addPacMan(new Location(9, 11));
-                Ghost ghost = frame.addGhost(new Location(10, 11), "Ghostie", Color.red);
+        PacMan pacman = frame.addPacMan(new Location(9, 11));
+        Ghost ghost = frame.addGhost(new Location(10, 11), "Ghostie", Color.red);
 
                 
 
 		assertTrue(frame.getMap().move("pacman", new Location (9, 12), Map.Type.PACMAN));
 		assertFalse(frame.getMap().getLoc( new Location(9, 11)).contains(Map.Type.PACMAN));
-
-	assertTrue(frame.getMap().getLoc(new Location(9, 12)).contains(Map.Type.PACMAN));
+		assertTrue(frame.getMap().getLoc(new Location(9, 12)).contains(Map.Type.PACMAN));
 
 
 	}
